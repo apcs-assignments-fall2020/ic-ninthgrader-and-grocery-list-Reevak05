1b5fb4613f11175cb0d98808ab454cec944cb6cc
Reevak05 <kaveergera@gmail.com>
2020-11-03 09:15:32 -0800

    Create GroceryList class and GroceryListClient.java

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,35 @@
 public class GroceryList {
-    
+    private String[] list;
+
+    public GroceryList() {
+        this.list = new String[10];
+    }
+
+    public void add(String addition) {
+        for (int i = 0; i<this.list.length; i++) {
+            if (this.list[i] == null) {
+                this.list[i] = addition;
+                break;
+            }
+        }
+    }
+
+    public void remove(String removal) {
+        for (int i = 0; i<this.list.length; i++) {
+            if (this.list[i].equals(removal)) {
+                this.list[i] = null;
+                break;
+            }
+        }
+    }
+
+    public String toString() {
+        String str = "";
+        for(int i = 0; i<this.list.length; i++) {
+            if (!(this.list[i] == null)) {
+                str += this.list[i] + ", ";
+            }
+        }
+        return str;
+    }
 }
