10450784b8fe6a780654ce73734800b44a050296
Reevak05 <kaveergera@gmail.com>
2020-11-03 08:40:36 -0800

    Populate NinthGrader class

--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,20 @@
-public class NinthGrader {
-    
+public class NinthGrader extends Student {
+
+    public NinthGrader(String firstName, String lastName) {
+        super(firstName, lastName, 2024);
+    }
+
+    public NinthGrader() {
+        super("John", "Doe", 2024);
+    }
+
+    @Override
+    public void doSomething() {
+        System.out.println("Hello! I am in 9th grade.");
+    }
+
+    @Override
+    public String toString() {
+        return this.getFirstName() + " " + this.getLastName() + ", 2024 (Ninth Grader)";
+    }
 }
